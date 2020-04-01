5c47f9941d27c8555b6ae83ccbc74eebf37d9fd2
car69 <car69@cs-ssh.argo.uwf.edu>
2020-03-31 22:00:39 -0500

    working connection

--- chatServer.c
+++ chatServer.c
@@ -16,9 +16,19 @@
 
 #define PORT_NUMBER 60001
 #define DATA_SIZE 2048
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 100
 
-/* Client Structure */
+/* Client structure */
+typedef struct {
+    struct sockaddr_in addr; /* Client remote address */
+    int connfd;              /* Connection file descriptor */
+    int uid;                 /* Client unique identifier */
+    char name[32];           /* Client name */
+} client_t;
+
+client_t *clients[MAX_CLIENTS];
+
+
 
 /* Add client to queue */
 
@@ -36,12 +46,12 @@
 
 /* Handles Server starting sequence */
 int main() {
-	int listenfd = 0; connfd = 0; 
+	int listenfd = 0, connfd = 0; 
 	struct sockaddr_in server_addr;
-	struct sockaddr_in clinet_addr;
+	struct sockaddr_in client_addr;
 	
 	// Server settings
-	int listenfd = socket(AF_INET,SOCK_STREAM,0);
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
 	if(listenfd < 0)
 	{
 		perror("fail");
@@ -62,10 +72,10 @@ int main() {
 	}
 
 	// Listening for connections
-	if(listen(server_fd, MAX_CONNECTIONS) == -1)
+	if(listen(listenfd, MAX_CLIENTS) == -1)
 	{
 		perror("Socket listing failed");
-		return EXIT_FAILURE
+		return EXIT_FAILURE;
 	}
 
 	printf("-=| Server started |=-\n");
@@ -74,9 +84,9 @@ int main() {
 	while(1) 
 	{
 		// Connecting client to server
-		socklen_t client_len = sizeof(client_addr)
-		connfd = accept(listenfd, (struct sockaddr*)&client_addr, &sin_len);
-		if(client_fd == -1)
+		socklen_t client_len = sizeof(client_addr);
+		connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_len);
+		if(connfd == -1)
 		{
 			perror("connection fail....\n");
 			continue;
@@ -85,15 +95,17 @@ int main() {
 			
 		// Send Data
 		char buff[2047] = "";
-		long valread = read(client_fd, buff, 2047);
-
+		long valread = read(connfd, buff, 2047);
 		if(valread > 0) {
 			char data[2047] = "Hello from server";
 			printf(buff);		
-			send(client_fd, data, sizeof(data), 0); // Then send formatted response back to client
+			send(connfd, data, sizeof(data), 0); // Then send formatted response back to client
 		}
+
+
+		sleep(1);
 	}
-	close(server_fd);
+	return EXIT_SUCCESS;
 
 	return 0;
 }
